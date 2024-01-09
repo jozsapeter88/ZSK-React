2ef469afe035f2d4c842dfe0e2d95282ce7b834d
jozsapeter88 <jozsapeter88@gmail.com>
2024-01-09 14:03:38 +0100

    add BackToTopButton component

--- src/views/home.js
+++ src/views/home.js
@@ -12,6 +12,7 @@ import Review from '../components/review'
 import Article from '../components/article'
 import './home.css'
 import FAQ from '../components/faq'
+import BackToTopButton from '../components/backtotop'
 
 const Home = (props) => {
   return (
@@ -229,7 +230,7 @@ const Home = (props) => {
             </header>
             <div className="home-plans-container">
               <main className="home-plans">
-                <div className="home-plan1">
+                <div className="home-plan0" style={{ height: "80vh"}}>
                   <div className="home-details">
                     <div className="home-header03">
                       <label className="home-name">Ping-Gold</label>
@@ -299,7 +300,7 @@ const Home = (props) => {
                     <br></br>
                     <br></br>
                     <br></br>
-                    <button className="buyBtn" onClick="">
+                    <button className="buyBtn">
                       <span>Vásárlás</span>
                     </button>
                   </dialog>
@@ -368,7 +369,7 @@ const Home = (props) => {
                     >
                       ❌
                     </button>
-                    <button className="buyBtn" onClick="">
+                    <button className="buyBtn">
                       <span>Vásárlás</span>
                     </button>
                     <br></br>
@@ -448,7 +449,7 @@ const Home = (props) => {
                     >
                       ❌
                     </button>
-                    <button className="buyBtn" onClick="">
+                    <button className="buyBtn">
                       <span>Vásárlás</span>
                     </button>
                     <br></br>
@@ -511,7 +512,7 @@ const Home = (props) => {
                     >
                       ❌
                     </button>
-                    <button className="buyBtn" onClick="">
+                    <button className="buyBtn">
                       <span>Vásárlás</span>
                     </button>
                     <br></br>
@@ -572,7 +573,7 @@ const Home = (props) => {
                     >
                       ❌
                     </button>
-                    <button className="buyBtn" onClick="">
+                    <button className="buyBtn">
                       <span>Vásárlás</span>
                     </button>
                     <br></br>
@@ -633,7 +634,7 @@ const Home = (props) => {
                     >
                       ❌
                     </button>
-                    <button className="buyBtn" onClick="">
+                    <button className="buyBtn">
                       <span>Vásárlás</span>
                     </button>
                     <br></br>
@@ -697,7 +698,7 @@ const Home = (props) => {
                       a szolgáltatás, létrehoztunk egy Telegram beszélgetős
                       csoportot, ahol a{" "}
                       <span className="colorful-text2"> Pénzdoki kurzust </span>
-                      (logoszinu legyen ez a ket szo) elvégzett társakkal meg
+                      elvégzett társakkal meg
                       tudjátok beszélni meglátásaitokat, elemzéseiteket és több
                       szem többet lát alapon, akár közösen is fogadhattok,
                       profitálhattok. Nem mellesleg a csoportban
@@ -740,7 +741,7 @@ const Home = (props) => {
                   >
                     ❌
                   </button>
-                  <button className="buyBtn" onClick="">
+                  <button className="buyBtn">
                     <span>Vásárlás</span>
                   </button>
                   <br></br>
@@ -813,7 +814,7 @@ const Home = (props) => {
                   >
                     ❌
                   </button>
-                  <button className="buyBtn" onClick="">
+                  <button className="buyBtn">
                     <span>Vásárlás</span>
                   </button>
                   <br></br>
@@ -866,7 +867,7 @@ const Home = (props) => {
                   >
                     ❌
                   </button>
-                  <button className="buyBtn" onClick="">
+                  <button className="buyBtn">
                     <span>Vásárlás</span>
                   </button>
                   <br></br>
@@ -1230,13 +1231,7 @@ const Home = (props) => {
           </section>
         </div>
       </footer>
-      <div className="home-slide-to-top">
-        <a href="#top" className="home-link5">
-          <svg viewBox="0 0 1024 1024" className="home-icon20">
-            <path d="M981.333 512c0-129.579-52.565-246.997-137.472-331.861s-202.283-137.472-331.861-137.472-246.997 52.565-331.861 137.472-137.472 202.283-137.472 331.861 52.565 246.997 137.472 331.861 202.283 137.472 331.861 137.472 246.997-52.565 331.861-137.472 137.472-202.283 137.472-331.861zM896 512c0 106.069-42.923 201.984-112.469 271.531s-165.461 112.469-271.531 112.469-201.984-42.923-271.531-112.469-112.469-165.461-112.469-271.531 42.923-201.984 112.469-271.531 165.461-112.469 271.531-112.469 201.984 42.923 271.531 112.469 112.469 165.461 112.469 271.531zM554.667 682.667v-238.336l97.835 97.835c16.683 16.683 43.691 16.683 60.331 0s16.683-43.691 0-60.331l-170.667-170.667c-4.096-4.096-8.789-7.168-13.824-9.259s-10.539-3.243-16.341-3.243c-5.547 0-11.136 1.067-16.341 3.243-5.035 2.091-9.728 5.163-13.824 9.259l-170.667 170.667c-16.683 16.683-16.683 43.691 0 60.331s43.691 16.683 60.331 0l97.835-97.835v238.336c0 23.552 19.115 42.667 42.667 42.667s42.667-19.115 42.667-42.667z"></path>
-          </svg>
-        </a>
-      </div>
+      <BackToTopButton />
     </div>
   );
 }
