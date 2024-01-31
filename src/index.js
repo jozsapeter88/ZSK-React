3bf2a7d24dccc454f4ee2d53da31f04d7fcab645
jozsapeter88 <jozsa.peter88@gmail.com>
2024-01-31 16:32:48 +0100

    remove stripe integration

--- src/index.js
+++ src/index.js
@@ -4,23 +4,17 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Redirect,
 } from "react-router-dom";
 
-import "@stripe/stripe-js";
 import "./style.css";
 import Home from "./views/home";
 import NotFound from "./views/not-found";
-import Success from "./views/success";
-import Cancel from "./views/cancel";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route component={Home} exact path="/" />
-        <Route component={Success} exact path="/success" />
-        <Route component={Cancel} exact path="/cancel" />
         <Route component={NotFound} />
       </Switch>
     </Router>
